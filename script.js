a918aa1abcbde76bbea54e413a880c7c30fb5a54
adifkhan <pkadif@gmail.com>
2023-08-16 03:40:19 +0600

    banner component implemented

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".skills",{
+    strings:['HTML-5, CSS-3','JavaScript, ES6','React, NextJs', 'Tailwind, Bootstrap, MaterialUI','NodeJs, ExpresJs', 'MongoDb, Mongoose','Firebase Authentication', 'JSON Web Token' ], 
+    typeSpeed:30,
+    backSpeed:30,
+    backDelay:500,
+    loop:true
+})
